746ec2ea5e80246535d04aef827e9079268fae01
wzy <2630559606@qq.com>
2023-04-13 18:45:00 +0800

    第一次提交

---
+++ src/components/welcome/LoginPage.vue
@@ -0,0 +1,91 @@
+<template>
+  <div style="text-align: center;margin: 0 20px">
+    <!--登录标题-->
+    <div style="margin-top: 125px">
+      <div style="font-size: 25px;font-weight: bold;margin-top: 10px">登录</div>
+      <div style="font-size: 18px;color: grey;margin-top: 10px">在进入系统之前请先进行登录</div>
+    </div>
+    <div style="margin-top: 30px">
+      <el-input v-model="form.username" type="text" placeholder="邮箱">
+        <template #prefix>
+          <el-icon>
+            <User/>
+          </el-icon>
+        </template>
+      </el-input>
+      <el-input v-model="form.password"
+                :type="form.showPassword ? 'text' : 'password'"
+                style="margin-top: 10px"
+                placeholder="密码">
+        <template #prefix>
+          <el-icon>
+            <Lock/>
+          </el-icon>
+        </template>
+        <template #suffix>
+          <el-icon @click="form.showPassword = !form.showPassword">
+            <View/>
+          </el-icon>
+        </template>
+      </el-input>
+    </div>
+    <el-row style="margin-top: 5px">
+      <el-col :span="12" style="text-align: left">
+        <el-checkbox v-model="form.remember" label="记住我"/>
+      </el-col>
+      <el-col :span="12" style="text-align: right">
+        <el-link @click="router.push('/forget')">忘记密码？</el-link>
+      </el-col>
+    </el-row>
+    <div style="margin-top: 40px">
+      <el-button @click="login()" style="width: 270px" type="success" plain>立即登录</el-button>
+    </div>
+    <el-divider>
+      <span style="color: grey;font-size: 13px">没有账号</span>
+    </el-divider>
+    <div>
+      <el-button style="width: 270px" @click="router.push('/register')" type="warning" plain>注册账号</el-button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {Lock, User, View} from '@element-plus/icons-vue'
+import {reactive} from "vue";
+import axios from 'axios';
+import {ElMessage} from "element-plus";
+import router from "@/router";
+
+const form = reactive({
+  username: '',
+  password: '',
+  remember: false,
+  showPassword: false
+})
+
+const login = () => {
+  if (!form.username || !form.password) {
+    ElMessage.warning('请填写用户名和密码！');
+  } else {
+    axios.post('/user/login', {
+      account: form.username,
+      password: form.password,
+    }).then(response => {
+      const isOk = response.data.code;
+      if(isOk === 0){
+        ElMessage.error("登录失败，请重试");
+      } else {
+        const message = response.data.data;
+        ElMessage.success(message);
+        router.push('/forget');
+      }
+    }).catch(error => {
+      console.error(error);
+    });
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,63 @@
+<template>
+  <div style="width: 100vw;height: 100vh;overflow: hidden;display: flex">
+    <div style="flex: 1">
+      <el-image style="width: 100%;height: 100%" fit="cover"
+                :src="'loginImg.jpg'"/>
+    </div>
+    <div class="welcome-title">
+      <div style="font-size: 30px;font-weight: bold">欢迎来到我的GPT平台</div>
+      <div style="margin-top: 10px">在这里你可以使用chatGPT,本网站主要用于个人学习。</div>
+      <div style="margin-top: 5px">在这个网站中我在这里会尽情的发挥自己的所学。</div>
+      <div style="margin-top: 5px">也欢迎您的到来发表自己对这个网站的意见。</div>
+      <div style="margin-top: 5px" v-html="renderTextItBaiMa"></div>
+      <div style="margin-top: 5px" v-html="renderTextBiBi"></div>
+    </div>
+    <div style="width: 400px;background-color: white;z-index: 1">
+      <router-view v-slot="{ Component }">
+        <transition name="el-fade-in-linear" mode="out-in">
+          <component :is="Component" style="height: 100%"/>
+        </transition>
+      </router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import { reactive, computed } from 'vue';
+export default {
+  setup() {
+    const state = reactive({
+      text1: '前端的登录页面参考了白马程序员,在此鸣谢每位it大佬的无私奉献',
+      url1: 'https://itbaima.net',
+      text2: '在此鸣谢itbaima以及每位it大佬的无私奉献。',
+      url2: 'https://space.bilibili.com/37737161'
+    });
+
+    const renderTextItBaiMa = computed(() => {
+      const reg = /白马程序员/g; // 匹配需要转换为超链接的文本
+      return state.text1.replace(reg, `<a href="${state.url1}" target="_blank">白马程序员</a>`);
+    });
+
+    const renderTextBiBi = computed(() => {
+      const reg = /itbaima/g; // 匹配需要转换为超链接的文本
+      return state.text2.replace(reg, `<a href="${state.url2}" target="_blank">renderTextBiBi</a>`);
+    });
+
+    return {
+      renderTextItBaiMa,
+      renderTextBiBi
+    };
+  }
+};
+</script>
+
+
+<style scoped>
+.welcome-title {
+  position: absolute;
+  bottom: 30px;
+  left: 30px;
+  color: white;
+  text-shadow: 0 0 10px black;
+}
+</style>
