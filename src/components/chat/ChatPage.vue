54e78379c85dfe12d64a7db08c62cbc0c91fd825
wzy <2630559606@qq.com>
2023-04-19 21:12:29 +0800

    难死我了，实现了简单的展示后端对话，但有一些bug，有待优化

--- src/components/chat/ChatPage.vue
+++ src/components/chat/ChatPage.vue
@@ -3,23 +3,17 @@
     <div v-for="item in curContext" :key="item.id">
       <el-card class="box-card">
         <template #header>
-          <template>
-            <el-icon>
-              <User/>
-            </el-icon>
-            用户
-          </template>
+          <el-icon>
+            <User/>
+          </el-icon>
+          用户
         </template>
-        {{ item.usercomment }}
+        {{ item.usercomment  }}
       </el-card>
       <el-card class="box-card">
         <template #header>
-          <template>
-            <el-icon>
-              <User/>
-            </el-icon>
-            chatgpt
-          </template>
+          <img src="public/gpt-img.png" alt="">
+          ChatGPT
         </template>
         {{ item.gptcomment }}
       </el-card>
@@ -29,7 +23,7 @@
 
 <script setup>
 import {ElMessage} from 'element-plus';
-import {onMounted, reactive} from 'vue'
+import {onMounted, reactive, watchEffect, ref} from 'vue'
 import {User} from "@element-plus/icons-vue";
 import {useStore} from "@/stores";
 import axios from 'axios'
@@ -37,7 +31,25 @@ import axios from 'axios'
 const store = useStore()
 let curContext = reactive([]);
 
+// 页面初始化执行的方法
 onMounted(() => {
+  getData();
+})
+
+import { onBeforeRouteUpdate } from 'vue-router'
+
+onBeforeRouteUpdate((to, from) => {
+  if (isChatRoute(to.path)) {
+    getData()
+  }
+})
+
+function isChatRoute(path) {
+  const regex = /^\/chat\/\d{18}$/
+  return regex.test(path)
+}
+
+function getData() {
   store.arr.splice(0, store.arr.length);
   axios.get('/comment/getCommentDetail', {
     headers: {
@@ -52,14 +64,15 @@ onMounted(() => {
     if (response.data.code === 1) {
       curContext = response.data.data
       console.log(curContext)
+      ElMessage.success("~~~数据获取成功~~")
       curContext.map(item => {
         addData(item.id, item.usercomment, item.gptcomment)
       })
     } else {
-      ElMessage.success("出现错误，请稍后再试")
+      ElMessage.error("出现错误，请稍后再试")
     }
   })
-})
+}
 
 // 添加一组数据
 function addData(id, usercomment, gptcomment) {
@@ -87,7 +100,7 @@ function addData(id, usercomment, gptcomment) {
 }
 
 .box-card {
-  width: 800px;
+  width: 650px;
   margin-bottom: 20px;
   margin-left: 300px;
   margin-right: 300px;
